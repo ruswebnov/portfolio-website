253e5a517239eee1c785de094fd7631ae457592f
Ruslan <ruslantrade2019@gmail.com>
2020-07-11 00:00:39 +0700

    ready mobile first

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
 
+  /* Анимация меню */
   $(window).on('scroll', function() {
     if($(window).scrollTop()){
       $('.header__menu').addClass('black');
@@ -7,14 +8,9 @@ $(document).ready(function(){
     else {
       $('.header__menu').removeClass('black');
     }
-
-
-  
-
-
   });
   
-  
+  /* Скрипт для кнопки */
   $('.menu-btn').click( function() {
     console.log(this);
     $(this).toggleClass("active");
@@ -23,8 +19,36 @@ $(document).ready(function(){
     }else{
       $('nav').slideToggle();
     }
+
+    /* Закрывает мобилльное меню при клике на раздел */
+    $('.header__nav-link').click(function(){
+      $('nav').slideUp();
+    })
   });
 
+
+  /* Swiper слайдер секция skills */
+  let swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: false,
+    slidesPerView: 'auto',
+    slideToClickedSlide: true,
+     autoplay: {
+       delay: 3000,
+     },
+   
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  });  
 });
 
 
